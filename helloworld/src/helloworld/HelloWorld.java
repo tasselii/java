e79d33febe77bdd668bb32f7dfd97b97871ac5d4
tasselii <thiagotasseli@outlook.com.br>
2025-04-11 13:55:27 -0300

    :tada: Commit Incial

---
+++ helloworld/src/helloworld/HelloWorld.java
@@ -0,0 +1,12 @@
+
+package helloworld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		System.out.print("Olá mundo\n"); // "\n" pula uma linha na impressão
+		System.out.println("A turma Java 82 é TOP");
+	}
+
+}
