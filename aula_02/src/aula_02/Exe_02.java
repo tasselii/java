4f9f98f4203c5b02c4ef725a3a9a0a7e76170842
tasselii <thiagotasseli@outlook.com.br>
2025-04-14 12:07:08 -0300

    :sparkle: Adicionando os Exercicios do CANVAS

--- aula_02/src/aula_02/Exe_02.java
+++ aula_02/src/aula_02/Exe_02.java
@@ -1,5 +1,28 @@
 package aula_02;
 
-public class Exe_02 {
+import java.util.Scanner;
 
+public class Exe_02 {
+	
+	public static void main(String[] args) {
+	
+		Scanner leia = new Scanner(System.in);
+	
+    		System.out.print("Digite a nota 1: ");
+    		float nota_01 = leia.nextFloat();
+    		
+    		System.out.print("Digite a nota 2: ");
+    		float nota_02 = leia.nextFloat();
+    		
+    		System.out.print("Digite a nota 3: ");
+    		float nota_03 = leia.nextFloat();
+    	
+    		System.out.print("Digite a nota 4: ");
+    		float nota_04 = leia.nextFloat();
+    		
+    		float media = (nota_01 + nota_02 + nota_03 + nota_04) / 4;
+    		
+    	System.out.printf("Média final: %.1f", media);
+
+	}
 }
--- aula_02/src/aula_02/Exe_04.java
+++ aula_02/src/aula_02/Exe_04.java
@@ -1,5 +1,28 @@
 package aula_02;
 
+import java.util.Scanner;
+
 public class Exe_04 {
 
+	public static void main(String[] args) {
+		
+		Scanner leia = new Scanner(System.in); 
+		
+			System.out.print("Digite o numero 1: ");
+			float numero_01 = leia.nextFloat();
+			
+			System.out.print("Digite o numero 2: ");
+			float numero_02 = leia.nextFloat();
+			
+			System.out.print("Digite o numero 3: ");
+			float numero_03 = leia.nextFloat();
+			
+			System.out.print("Digite o numero 4: ");
+			float numero_04 = leia.nextFloat();
+			
+			float calculo = (numero_01 * numero_02)- (numero_03 * numero_04);
+			
+			System.out.printf("Diferença: %.2f", calculo);
+			
+	}
 }
